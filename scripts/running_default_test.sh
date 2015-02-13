506a6768973394ded9f3019be3f6c83d28d35e81
helojagt <helojagt@gmail.com>
2015-02-12 21:56:58 -0600

    Removing hardcoded variables

--- scripts/running_default_test.sh
+++ scripts/running_default_test.sh
@@ -9,34 +9,31 @@
 # project                              #
 ########################################
 
+#Variables
+DEFAULT_JOB='default.ini'
+BANDWIDTH_OUT='bandwidth_test.out'
+LATENCY_OUT='latency_test.out'
+CPUCLOCK_OUT='cpuclock_test.out'
+DEBUG='debug_test.out'
+
 #Doing the standard tests
 
-fio --output=debug_test.out --debug=all default.ini
-
-fio --output=bandwidth_test.out --bandwidth-log default.ini
-
-fio --output=latency_test.out default.ini
-
-fio --cpuclock-test --output=cpuclock_test.out
+fio --output=$DEBUG --debug=all $DEFAULT_JOB
+fio --output=$BANDWIDTH_OUT --bandwidth-log $DEFAULT_JOB
+fio --output=$LATENCY_OUT $DEFAULT_JOB
+fio --cpuclock-test --output=$CPUCLOCK_OUT
 
 #Accesing the output directory
 
 cd ..
-
 export DIR=$(pwd)
-
 mkdir output
-
 cd $DIR/scripts
 
 #Coping the files to the output directory
 
-mv debug_test.out $DIR/output
-
-mv bandwidth_test.out $DIR/output
-
-mv cpuclock_test.out $DIR/output
-
-mv latency_test.out $DIR/output
-
+mv $DEBUG $DIR/output
+mv $BANDWIDTH_OUT $DIR/output
+mv $CPUCLOCK_OUT $DIR/output
+mv $LATENCY_OUT $DIR/output
 echo "Test were succesful, the outputs are in the output directory located on the linux_perfomance directory."

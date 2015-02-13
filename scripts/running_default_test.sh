1e5bc0dc9cc8a22b5f00b703b7b640722154904f
helojagt <helojagt@gmail.com>
2015-02-12 22:03:04 -0600

    Spaces eliminated

--- scripts/running_default_test.sh
+++ scripts/running_default_test.sh
@@ -17,23 +17,21 @@ CPUCLOCK_OUT='cpuclock_test.out'
 DEBUG='debug_test.out'
 
 #Doing the standard tests
-
 fio --output=$DEBUG --debug=all $DEFAULT_JOB
 fio --output=$BANDWIDTH_OUT --bandwidth-log $DEFAULT_JOB
 fio --output=$LATENCY_OUT $DEFAULT_JOB
 fio --cpuclock-test --output=$CPUCLOCK_OUT
 
 #Accesing the output directory
-
 cd ..
 export DIR=$(pwd)
 mkdir output
 cd $DIR/scripts
 
 #Coping the files to the output directory
-
 mv $DEBUG $DIR/output
 mv $BANDWIDTH_OUT $DIR/output
 mv $CPUCLOCK_OUT $DIR/output
 mv $LATENCY_OUT $DIR/output
+
 echo "Test were succesful, the outputs are in the output directory located on the linux_perfomance directory."

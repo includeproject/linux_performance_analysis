df3ba0c49bd4a7a97807e978f0a9a105771fc35a
JoyceLA <joycseleneanaid@gmail.com>
2015-02-26 21:14:47 -0600

    Link pages

---
+++ web_scripts/Linux_performance_tests_page/pages/login.php
@@ -0,0 +1,41 @@
+<?php
+ session_start(); 
+// al volver al index si existe una session, esta sera destruida, existen formas de conservarlas como con un if(session_start()!= NULL). Pero por el momento para el ejemplo no es valido. 
+session_unset();
+ 
+session_destroy(); 
+// Se destruye la session existente de esta forma no permite el duplicado.
+ ?>
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Admin Login</title>
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="../assets/styles.css" rel="stylesheet" media="screen">
+     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+  </head>
+  <body id="login">
+    <div class="container">
+
+      <form class="form-signin" action="user_panel.php" method="POST">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input type="text" class="input-block-level" placeholder="Email address" name="username">
+        <input type="password" class="input-block-level" placeholder="Password" name="pass">
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-large btn-primary" type="submit" value="Crear sesión">Sign in</button>
+      </form>
+
+    </div> <!-- /container -->
+    <script src="vendors/jquery-1.9.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>

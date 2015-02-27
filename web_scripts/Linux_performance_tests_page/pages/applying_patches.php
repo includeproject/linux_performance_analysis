7f662d5f50391ca9287058c6555beb3292af5bf0
Iván Martínez <oima.91@gmail.com>
2015-02-27 16:15:49 -0600

    Files into directory

--- web_scripts/Linux_performance_tests_page/pages/applying_patches.php
+++ web_scripts/Linux_performance_tests_page/pages/applying_patches.php
@@ -1,10 +1,8 @@
 <?php
-     /* Empezamos la sesión */
-     session_start();
-     /* Si no hay una sesión creada, redireccionar al index. */
-     if(empty($_SESSION['username'])) { // Recuerda usar corchetes.
-        header('Location: login.php');
-     }
+session_start();
+if (empty($_SESSION['username'])) { 
+    header('Location: login.php');
+}
 ?>
 <!DOCTYPE html>
 <html class="no-js">
@@ -13,7 +11,6 @@
         <title>Linux Analysis Performance</title>
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-        <!--<link href="../vendors/easypiechart/jquery.easy-pie-chart.css" rel="stylesheet" media="screen">-->
         <link href="../assets/styles.css" rel="stylesheet" media="screen">
         <script src="../vendors/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
@@ -30,7 +27,7 @@
                     <ul class="nav pull-right">
                         <li class="dropdown">
                             <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> 
-                                <i class="icon-user"></i><?=$_SESSION['username'];?> <i class="caret"></i>
+                                <i class="icon-user"></i><?= $_SESSION['username']; ?> <i class="caret"></i>
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
@@ -51,9 +48,8 @@
             <?php include './../scripts/nav-bar.php'; ?>
             <div class="span9" id="content">
                 <div class="row-fluid">
-<!--<<<<<<< HEAD-->
                     <form method="post" role="form" action="./../scripts/apply_patch.php" enctype="multipart/form-data">
-                        <div class="jumbotron">
+                        <div class="jumbodtron">
                             <h1>
                                 Upload your patches to analyze!
                             </h1>
@@ -62,66 +58,11 @@
                                 The results will be shown into another page after the operations end. You are able to upload 
                                 patches only with extension .diff and .patch.
                             </p>
-                            <p>
-<!--=======-->
-                    <div class="jumbodtron">
-                        <h1>
-                            Upload your patches to analyze!
-                        </h1>
-                        <p>
-                            You are able to upload your own patches and analyze their performance with ltp. 
-                            The results will be shown into another page after the operations end. You are able to upload 
-                            patches only with extension .diff and .patch.
-                        </p>
-                        <br/>
-                        <br/>
-                        <form id="fileupload" action="http://jquery-file-upload.appspot.com/" method="POST" enctype="multipart/form-data" class="">
-                            <!-- Redirect browsers with JavaScript disabled to the origin page -->
-                            <noscript>&lt;input type="hidden" name="redirect" value="https://blueimp.github.io/jQuery-File-Upload/"&gt;</noscript>
-                            <!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-                            <div class="row fileupload-buttonbar">
-                                <div class="col-lg-7">
-                                    <!-- The fileinput-button span is used to style the file input field as button -->
-                                    <span class="btn btn-success fileinput-button">
-                                        <i class="glyphicon glyphicon-plus"></i>
-                                        <span>Add files...</span>
-                                        <input type="file" name="files[]" multiple="">
-                                    </span>
-                                    <button type="submit" class="btn btn-primary start" onclick="location.href='execute_script.php'">
-                                        <i class="glyphicon glyphicon-upload"></i>
-                                        <span>Start upload</span>
-                                    </button>
-                                    <!--                                    <button type="reset" class="btn btn-warning cancel">
-                                                                            <i class="glyphicon glyphicon-ban-circle"></i>
-                                                                            <span>Cancel upload</span>
-                                                                        </button>-->
-                                    <!--                                    <button type="button" class="btn btn-danger delete">
-                                                                            <i class="glyphicon glyphicon-trash"></i>
-                                                                            <span>Delete</span>
-                                                                        </button>-->
-                                                                        <!--<input type="checkbox" class="toggle">-->
-                                    <!-- The global file processing state -->
-                                    <span class="fileupload-process"></span>
-                                </div>
-                                <!-- The global progress state -->
-                                <div class="col-lg-5 fileupload-progress fade">
-                                    <!-- The global progress bar -->
-                                    <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-                                        <div class="progress-bar progress-bar-success" style="width:0%;"></div>
-                                    </div>
-                                    <!-- The extended global progress state -->
-                                    <div class="progress-extended">&nbsp;</div>
-                                </div>
-                            </div>
-                            <!-- The table listing the files available for upload/download -->
-                            <table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
-                        </form>
-<!-->>>>>>> origin/master-->
-
-                                <span class="btn btn-primary btn-file">Upload a list of patches to analyze
-                                    <input class="file" id="fileInput" type="file" name="files[]" multiple/>
-                                </span>
-                            </p>
+                            <br/>
+                            <br/>
+                            <span class="btn btn-primary btn-file">Upload a list of patches to analyze
+                                <input class="file" id="fileInput" type="file" name="files[]" multiple/>
+                            </span>
                         </div>
                         <table class="table">
                             <thead>
@@ -145,7 +86,6 @@
                             </tbody>
                         </table>
                         <div id="alert-box" class="alert alert-success">
-                            <!--<button type="button" class="close" data-dismiss="alert">&times;</button>-->
                             <h4 id="alert-title">Accepted files</h4>
                             <p id="alert-message">The operation can be completed successfully</p>
                             <button type="submit" class="btn btn-inverse" id="submit">
@@ -153,8 +93,6 @@
                             </button>
                         </div>
                     </form>
-
-
                     <div id="alert-box-danger" class="alert alert-danger">
                         <button type="button" class="close" data-dismiss="alert">&times;</button>
                         <h4>There are some rejected files</h4>
@@ -171,7 +109,6 @@
     </div>
     <script src="./../vendors/jquery-1.9.1.min.js"></script>
     <script src="./../bootstrap/js/bootstrap.min.js"></script>
-    <!--<script src="../vendors/easypiechart/jquery.easy-pie-chart.js"></script>-->
     <script src="./../assets/scripts.js"></script>
     <script>
         function isSupportedExtension(extension) {
--- web_scripts/Linux_performance_tests_page/scripts/apply_patch.php
+++ web_scripts/Linux_performance_tests_page/scripts/apply_patch.php
@@ -2,11 +2,23 @@
 
 #This script must contain the code for catch, validate and apply the patches
 #sent by the user
-    echo $_FILES['files']["name"][0];
+#Please make sure that the $uploadDir directory exists
+#
+#This files are not validated on this release, please be careful what you upload
 
-fixFilesArray($_FILES['files']);
-foreach ($_FILES['files'] as $position => $file) {
-    // should output array with indices name, type, tmp_name, error, size
-    var_dump($file);
+$uploadDir = '/var/www/html/uploads/';
+$userDir = '';
+if ($_FILES['files']['name'] && $_FILES['files']['error']) {
+    $i = 0;
+    foreach ($_FILES['files']['name'] as $file) {
+        echo $file[$i].'<br/>';
+        if (copy($_FILES['files']['tmp_name'][$i], $uploadDir . $_FILES['files']['name'][$i])) {
+            echo "The files were succesfully uploaded. <br/>";
+        } else {
+            echo "An error occurred while uploading the file. <br/>";
+            echo 'error with '. $file[$i] .'<br/>';
+            echo 'Position: '. $i .'<br/>';
+        }
+        ++$i;
+    }
 }
-

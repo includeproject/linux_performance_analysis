c7ad3aa74cae84e01811b94470c0f30f9821b449
Iván Martínez <oima.91@gmail.com>
2015-04-07 00:12:41 -0500

    Form validation enhanced

--- web_scripts/Linux_performance_tests_page/pages/login.php
+++ web_scripts/Linux_performance_tests_page/pages/login.php
@@ -1,50 +1,51 @@
 <?php
- session_start(); 
-// al volver al index si existe una session, esta sera destruida, existen formas de conservarlas como con un if(session_start()!= NULL). Pero por el momento para el ejemplo no es valido. 
-
-//Sesion activa, redireccionar al panel
-if (isset($_SESSION['username']) || isset($_SESSION['id_user'])){
-  header("location: user_panel.php");
+session_start();
+if (isset($_SESSION['username']) && isset($_SESSION['id_user'])) {
+    header("location: user_panel.php");
+    die;
+} else {
+    $error = $_SESSION['error'];
+    $user = $_SESSION['user'];
 }
-$alert = $_SESSION['alert'];
-//Delete session variables when load page loggin
-unset($_SESSION['firstname']);
-unset($_SESSION['lastname']);
-unset($_SESSION['user']);
-unset($_SESSION['email']);
- ?>
+?>
 <!DOCTYPE html>
 
 <html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Admin Login</title>
-    <!-- Bootstrap -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-    <link href="../assets/styles.css" rel="stylesheet" media="screen">
-     
-    <script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-  </head>
-  <body id="login">
-    <div class="container">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Admin Login</title>
+        <!-- Bootstrap -->
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="../bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <link href="../assets/styles.css" rel="stylesheet" media="screen">
 
-      <form class="form-signin" action="../scripts/verify_account.php" method="POST">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" class="input-block-level" placeholder="User name or Email" name="username">
-        <input type="password" class="input-block-level" placeholder="Password" name="pass">
-         <div style="color:#FF0000">
-           <?php echo $alert ?>
+        <script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body id="login">
+        <div class="container">
+            <form class="form-signin" action="../scripts/session_security/verify_account.php" method="POST">
+                <h2 class="form-signin-heading">Please sign in</h2>
+                <div class="control-group <?php echo ($error != '') ? 'error' : ''; ?>">
+                    <label class="control-label"><?php echo ($error != '') ? $error : ''; ?></label>
+                    <div class="controls">
+                        <input type="text" class="input-block-level" placeholder="User name or Email" name="username" value="<?php echo $user ?>"/>
+                        <input type="password" class="input-block-level" placeholder="Password" name="pass">
+                    </div>
+                </div>
+                <div style="color:#FF0000">
+                    <?php echo $alert; ?>
+                </div>
+                <label class="checkbox">
+                    <input type="checkbox" value="remember-me" name="rememberme"> Remember me
+                </label>
+                <button class="btn btn-large btn-primary" name="login_user" type="submit" value="sign">Sign in</button>
+                <button class="btn btn-default btn-link pull-right" name="register" type="button" value="registin" onclick="location.href = 'user_register.php'">Register Account!</button>      
+            </form>
         </div>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-large btn-primary" name="login_user" type="submit" value="sign">Sign in</button>
-        <button class="btn btn-default btn-link pull-right" name="register" type="button" value="registin" onclick="location.href='user_register.php'">Register Account!</button>      
-      </form>
-      
-    </div> <!-- /container -->
-    <script src="vendors/jquery-1.9.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-  </body>
+        <script src="vendors/jquery-1.9.1.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
 </html>
+<?php
+unset($_SESSION['error']);
+unset($_SESSION['user']);

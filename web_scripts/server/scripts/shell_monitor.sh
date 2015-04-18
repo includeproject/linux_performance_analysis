9c9cc7a4ab4a4db65921bf03600bd386c04b8070
helojagt <helojagt@gmail.com>
2015-04-17 23:25:43 -0500

    Documentation

--- web_scripts/server/scripts/shell_monitor.sh
+++ web_scripts/server/scripts/shell_monitor.sh
@@ -2,6 +2,14 @@
 
 #set -x
 
+##############################################################################
+#           shell_monitor.sh                                                 #
+#    Monitors all the process send by the script start_jo.sh                 #
+#    In order to start the daemon run this script  before start a job        #
+#    Execute the script as follows: ". shell_monitor.sh &""                  #
+#                                                                            #
+##############################################################################
+
 # =============================================================================
 # Scripts Variables
 # =============================================================================
@@ -22,6 +30,8 @@ export secondsWaiting=15
 # Script Functions
 # =============================================================================
 
+#checkOnlineJob function
+#Checks if the PID is active using the "ps ax" program. If the PID is active, returns 0 else returns 1.
 checkOnlineJob (){
     jobPID=$1
     
@@ -32,7 +42,9 @@ checkOnlineJob (){
     fi   
 }
 
-
+#checkOnlineAll function
+#Uses while loop to test all the PIDs using the "checkOnlineJob" function, the value returned by this function ->
+#-> is stored on the corresponding slot on the "jobsActive" function.
 checkOnlineAll (){
     declare -i index=0
 
@@ -43,12 +55,17 @@ checkOnlineAll (){
     done	
 }
 
+#initializeJobsPIDs functions
+#Sets on the index "0" the value "arraLimit"
 initializeJobsPIDs (){
     if [[ ${jobsPIDs[0]} != $arrayLimit ]]; then
 	jobsPIDs[0]=$arrayLimit
     fi
 }
 
+#saveJobsStatus function
+#Using a while loop, saves the values of the "jobsActive" and "jobsName" vaiables -> 
+#-> on the file until the "jobsPIDs" reaches the "arrayLimit" value. 
 saveJobsStatus (){
     declare -i index=0
     rm /vagrant/status.log
@@ -59,6 +76,10 @@ saveJobsStatus (){
     done
 }
 
+#saveVariablesOnFile function
+#Does not wait for another lock file to terminate because the function only runs once on the begining of the script.
+#Sets a lock file. Saves the "jobsNames" and "jobsPIDs" variables to a file called "variable.file"
+#Delates the lock file.
 saveVariablesOnFile (){
     lockfile -r 0 shell_monitor.lock
     echo 'jobsNames=()' >> variable.file
@@ -67,7 +88,10 @@ saveVariablesOnFile (){
     rm -f shell_monitor.lock
 }
 
-readVariablesFromFile() {
+#readVariablesFromFile function
+#Waits for the star_job lock file to end. Set a lock file. 
+#Reads the file containing the variables. Delates the file lock. 
+readVariablesFromFile() { 
     while [[ -e start_job.lock ]]; do
 	sleep 1
     done
@@ -83,6 +107,10 @@ readVariablesFromFile() {
 # Script Main
 # =============================================================================
 
+#Main
+#disown the script so it's terminal session can be closed. Saves the variables already initialized.
+#Using a wile loop repets the following; read variables from file, checks the PIDs stored on "jobsPIDs" using "px ax", ->
+#-> Save the status of each application to "status.log"
 disown $!
 initializeJobsPIDs
 saveVariablesOnFile

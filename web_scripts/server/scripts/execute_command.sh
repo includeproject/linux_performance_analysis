7f571b6f96a952d1dd7de0037cde6491eb4401cc
Iván Martínez <oima.91@gmail.com>
2015-05-08 00:43:39 -0500

    script needed by apache to execute privileged commands

--- web_scripts/server/scripts/execute_command.sh
+++ web_scripts/server/scripts/execute_command.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/expect -f
+# usage: ./execute_command.sh user password command
+# @param user: must have the right permissions to execute the command
+# @param password: is the password of the user specified
+# @param command: this parameter must be in double quotes 
+# Command example
+# ./execute_command.sh kernel LinuxPerformance "fdisk -l > list_disks.txt"
 
 set timeout 10
 set user [lindex $argv 0]

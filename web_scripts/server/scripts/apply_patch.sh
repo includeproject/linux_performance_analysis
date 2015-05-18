c9aa1a7939b6d121aaf1527f6d3c3867baefbb22
Iván Martínez <oima.91@gmail.com>
2015-05-18 10:52:12 -0700

    Script that apply the kernel patches

--- web_scripts/server/scripts/apply_patch.sh
+++ web_scripts/server/scripts/apply_patch.sh
@@ -8,6 +8,7 @@ usage(){
 if [[ $# -lt 2 ]];
     then
     echo "Error, two arguments are expected. Check the manual for more information."
+    usage
     exit 1
     else
         if [[ ! -d $1 || ! -d $2 ]];
@@ -32,21 +33,18 @@ for file in $USER_PATCH_DIR; do
                      sudo export APPLY_PATCH="git apply $file";
 	      elif [[ $file == *.xz ]]
               then
-                     export APPLY_PATCH="sudo xz -dc $file | sudo patch -p1";
-                     sudo export APPLY_PATCH="sudo xz -dc $file | sudo patch -p1";
+                     export APPLY_PATCH="sudo xz -dc $file | $(sudo patch -p1 || sudo patch -p0)";
+                     sudo export APPLY_PATCH="sudo xz -dc $file | $(sudo patch -p1 || sudo patch -p0)";
               elif [[ $file == *.gz ]]
               then
-                     export APPLY_PATCH="sudo gunzip $file | sudo patch -p1";
-                     sudo export APPLY_PATCH="sudo xz -dc $file | sudo patch -p1";
+                     export APPLY_PATCH="sudo gunzip $file | $(sudo patch -p1 || sudo patch -p0)";
+                     sudo export APPLY_PATCH="sudo xz -dc $file | $(sudo patch -p1 || sudo patch -p0)";
               elif [[ $file == *.bz2 ]]
               then
-                     export APPLY_PATCH="sudo bunzip2 $file | sudo patch -p1";
-                     sudo export APPLY_PATCH="sudo xz -dc $file | sudo patch -p1";
+                     export APPLY_PATCH="sudo bunzip2 $file | $(sudo patch -p1 || sudo patch -p0)";
+                     sudo export APPLY_PATCH="sudo xz -dc $file | $(sudo patch -p1 || sudo patch -p0)";
               fi
       fi
       echo "Trying to apply your patches" >> ./logs/vm/vm_status
-      vagrant ssh -c "cd ~/linux && $APPLY_PATCH >> /vagrant/kernel/apply_patch_$local_patch_dir"
+      vagrant ssh -c "cd ~/linux && $APPLY_PATCH >> /vagrant/kernel/apply_patch"
 done;
-
-
-
